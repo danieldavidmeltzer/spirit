25cd76839e0d4c3041b370a8b2152d3a4181a341
Daniel Meltzer <meltzerapps@gmail.com>
2020-02-26 15:39:03 +0200

    Revert "wip"
    This reverts commit 6cd8130dda5e1caefd83da268a47eedffe2d4fff.

--- spirit/snapshot_parsing/snapshot_parsers/depth_image_parser.py
+++ spirit/snapshot_parsing/snapshot_parsers/depth_image_parser.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plotter
 
 
 def parse_depth_image(context, snapshot):
-    image_name = ''.join(random.choices(string.ascii_uppercase, k=36))
+    image_name = ''.join(random.choices(string.ascii_uppercase, k=32))
     path = context.path(image_name) + ".jpg"
     plotter.imsave(path, snapshot.depth_image, cmap="hot")
 
